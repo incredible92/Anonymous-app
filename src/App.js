1fc725084604d09cac2a2612129d8de88048769a
incredible92 <sulymanabdullahi@gmail.com>
2020-11-04 16:36:49 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from 'react-router-dom'
+import AnonMessenger from "./pages/Anon";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+import "./App.scss";
 
-function App() {
+const apiURL = 'https://ut23t.sse.codesandbox.io/api'
+
+export default function App() {
   return (
     <div className="App">
-      Hello World!
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={SignupPage} />
+        <Route path="/message/:anon" component={AnonMessenger} />
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
-
-export default App;
